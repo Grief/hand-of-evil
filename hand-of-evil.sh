900c0aa30ca4530225dcfb94f2a4137244563b26
Grief <iamgrief@gmail.com>
2016-03-30 23:35:36 +0300

    move all parameters to mapping.conf file

--- hand-of-evil.sh
+++ hand-of-evil.sh
@@ -2,70 +2,158 @@
 
 DIR="$(dirname "$(realpath "$0")")"
 ARCHIVE=HandOfEvil.zip
-TMP="${DIR}/hnd_tmp.png"
+TMP="${DIR}/tmp.png"
+TMP="${DIR}/tmp.png"
+MAPPING_CONF='mapping.conf'
+FIELDS=(name hot_point prefix frame)
+MESSAGES=(
+    [0]='There must be at least one Xcursor name'
+    [1]='Missing colon-separated hot point coordinates'
+    [2]='Missing file name prefix'
+    [3]='There must be at least one frame'
+    [100]='Invalid frame format'
+    [101]='No such image'
+)
+ERRORS=0
+LINE=1
+FILE_NAME='%s%s'
+FILE_MASK='%s%s'
 
-function prepare() {
-    s=(999 999 0 0)
-    for file in ../../hnd_$1*.png; do
-        d=(`convert ${file} -trim -print '%X %Y %w %h' ${TMP}`)
-        for i in 0 1; do
-            j=$((i + 2))
-            let d[j]+=d[i]
-            if (( d[i] < s[i] )); then s[i]=${d[i]}; fi
-            if (( d[j] > s[j] )); then s[j]=${d[j]}; fi
+function error() {
+    echo "LINE $LINE: $1"
+    let ERRORS++
+}
+
+function process() {
+    tmp=$(printf "${DIR}/tmp%04d.png" "${index}")
+    d=($(convert "${file}" -print '%W %H ' "${rotate_arg[@]}" "${do_args[@]}" -trim -print '%X %Y %w %h %W %H' "${tmp}" 2>&1))
+    if [ -n "${rotate_arg}" ]; then
+        hot_point=($(echo "${hot_point[@]} ${ANGLE} ${d[@]:0:2} ${d[@]:6:2}"|awk '{xd=$1-($4-1)/2; yd=$2-($5-1)/2; a=$3*atan2(0,-1)/180; c=cos(a); s=sin(a); printf("%.0f %.0f", ($6-1)/2+xd*c-yd*s, ($7-1)/2+yd*c+xd*s)}'))
+    fi
+    for i in 0 1; do
+        let hot_point[i]-=d[i+2]
+    done
+    unset extent
+    for i in 0 1; do
+        if (( hot_point[i] < 0 )); then
+            let d[i+4]-=hot_point[i]
+            hot_point[i]=0
+            extent=true
+        fi
+    done
+    if [ -n extent ]; then
+        mogrify -background none -gravity SouthEast -extent "${d[4]}x${d[5]}" "${tmp}"
+    fi
+    size=$(( d[4] > d[5] ? d[4] : d[5] ))
+    echo "${size} ${hot_point[0]} ${hot_point[1]} ${tmp}"
+}
+
+function conf() {
+    unset delay processed config
+    if [ -n "${DO}" ]; then
+        readarray -t do_args < <( echo ${DO[@]}|xargs -n 1 echo )
+    else
+        do_args=()
+    fi
+    if [ -n "${ANGLE}" ]; then
+        rotate_arg=( -background none -rotate "${ANGLE}" +repage)
+    else
+        rotate_arg=()
+    fi
+    index=1
+    max_size=0
+    declare -A processed
+    for ((f = 1; f <= $#; f++)); do
+        if [[ ! "${!f}" =~ ^(\*|[0-9]+)(:([0-9]+))?$ ]]; then return 100; fi
+        if [ -n "${BASH_REMATCH[3]}" ]; then delay="${BASH_REMATCH[3]}"; fi
+        suffix="${BASH_REMATCH[1]}"
+        if [ "${suffix}" = '*' ]; then
+            files=("$(printf "../../${FILE_MASK}" "${prefix}" '*')")
+        else
+            files=("$(printf "../../${FILE_NAME}" "${prefix}" "${suffix}")")
+        fi
+        for file in ${files[@]}; do
+            if [ -n "${processed["$file"]}" ]; then
+                if [ "${suffix}" != '*' ]; then
+                    config+=("${processed["$file"]} ${delay}")
+                fi
+                continue
+            fi
+            line=($(process))
+            size="${line[0]}"
+            if (( max_size < size )); then
+                let max_size=size
+            fi
+            line="${line[@]:1}"
+            config+=("${line} ${delay}")
+            processed["${file}"]="${line}"
+            let index++
         done
     done
-
-    for i in 0 1; do let s[i+2]-=s[i]; done
-
-    delays=(${@:4})
-    files=(../../hnd_$1*.png)
-    for (( i=0; i<${#files[@]}; i++ )); do
-        file=${files[i]}
-        if (( delays[i] != 0 )); then delay=${delays[i]}; fi
-        mogrify -crop ${s[2]}x${s[3]}+${s[0]}+${s[1]} ${file}
-        echo ${s[0]} $2 $3 ${file} ${delay}
+    for ((f = 0; f < "${#config[@]}"; f++)); do
+        echo ${max_size} "${config[f]}"
     done
 }
 
-function series() {
-    prepare $4 $2 $3 "${@:5}"|xcursorgen - $1
-}
-
-function single() {
-    file=../../hnd_$4.png
-    echo "`mogrify -trim -print '%[fx:max(w,h)]' ${file}` $2 $3 ${file}"|xcursorgen - $1
-}
-
-function rotated() {
-    convert -background none -rotate $5 ../../hnd_$4.png ${TMP}
-    single $1 $2 $3 tmp
-}
-
-function reorder() {
-    rm  ../../hnd_reordered*>/dev/null 2>&1
-    n=10
-    for i in "${@:2}"; do
-        cp ../../hnd_${1}00${i}.png ../../hnd_reordered${n}.png
-        let "n++"
+function map() {
+    unset name hot_point prefix frame
+    if [[ $# -eq 0 || $1 =~ ^#.* ]]; then return; fi
+    case $1 in
+        '!file-name') FILE_NAME="$2"; return ;;
+        '!file-mask') FILE_MASK="$2"; return ;;
+        '!do') DO=("${@:2}"); return ;;
+        '!rotate') ANGLE="$2"; return ;;
+        '!alias') ln -sf "$3" "../../$2"; return ;;
+        !*) error "Unknown option: ${1}"; return ;;
+    esac
+    for ((i = 1; i <= $#; i++)); do
+        if [[ -z "${hot_point}" && "${!i}" =~ ^([0-9]+):([0-9]+)$ ]]; then
+            hot_point=("${BASH_REMATCH[@]:1:2}")
+        elif [[ "${!i}" =~ (/|^\.\.?$) ]]; then
+            error "'${!i}' is invalid file name"; return
+        elif [ -n "${hot_point}" ]; then
+            prefix="${!i}"
+            if [ "$i" -lt $# ]; then frame=true; fi
+            break
+        elif [ "$i" -eq 1 ]; then
+            name="${!i}"
+        else
+            ln -sf "${name}" "${!i}"
+        fi
     done
+    for ((k = 0; k < ${#FIELDS[@]}; k++)); do
+        key="${FIELDS[k]}"
+        if [ -z "$(eval echo \$${key})" ]; then
+            error "${MESSAGES[k]}"; return
+        fi
+    done
+    config="$(conf "${@:i+1}")"
+    code=$?
+    if [ ${code} -eq 0 ]; then
+        echo "${config}"|xcursorgen - "${name}"
+        unset DO ANGLE
+    else
+        error "${MESSAGES[code]}"
+    fi
 }
 
 # Download and unpack archive with images
-mkdir -p "${DIR}/hand-of-evil/cursors"
+CURSORS="${DIR}/hand-of-evil/cursors"
+rm -r "${CURSORS}"
+mkdir -p "${CURSORS}"
 cd "${DIR}"
-if [ ! -f ${ARCHIVE} ]; then
-    wget -q -O ${ARCHIVE} --show-progress "ftp://ftp.ea-europe.com/support/patches/dk2/HandOfEvil.zip"
+if [ ! -f "${ARCHIVE}" ]; then
+    wget -q -O "${ARCHIVE}" --show-progress 'ftp://ftp.ea-europe.com/support/patches/dk2/HandOfEvil.zip'
     if [ $? != 0 ]; then
         echo "ERROR: Failed to download ${ARCHIVE} archive"
         exit -1
     fi
 fi
-if [ `md5sum ${ARCHIVE}|cut -d' ' -f1` != 'c1dd086f15a91bfa08c30530d0ff1e6f' ]; then
+if [ $(md5sum "${ARCHIVE}"|cut -d' ' -f1) != 'c1dd086f15a91bfa08c30530d0ff1e6f' ]; then
     echo "ERROR: ${ARCHIVE} archive checksum mismatch"
     exit -2
 fi
-unzip -q -o ${ARCHIVE}
+unzip -q -o "${ARCHIVE}"
 
 # Prepare layout and create index file
 cd hand-of-evil
@@ -74,73 +162,19 @@ echo "Name=Hand of Evil" >> index.theme
 echo "Inherits=dummy-theme" >> index.theme
 cd cursors
 
-# Some image modifications
-mogrify -draw 'point 30,32 point 31,31 point 32,32 point 31,33' ../../hnd_possess.png
-cp ../../hnd_holdcreature0042.png ../../hnd_dropcreature0042.png
+while read -ra words || [ -n "${words}" ]; do
+    map "${words[@]}"
+    let LINE++
+done < "${DIR}/${MAPPING_CONF}"
 
-# Reordered animations
-reorder dance 40 41 42 43 44 43 42 41
-series pointing_hand 0 9 reordered 600 60 60 60 30
-
-reorder dropcreature 42 43 44 45 46 47
-series zoom-out 8 62 reordered 300 80 80 80 80 300
-
-reorder dropcreature 42 47 46 45 44 43
-series zoom-in 8 62 reordered 300 300 80 80 80 80
-
-reorder grab 40 41 42 43 44 45 44 43 42 41
-series dnd-move 5 39 reordered 60
-
-# Animated cursors
-series wait           1 12 idle  60
-series pirate         0 64 slap  600 180 30
-series left_ptr_watch 1 16 dance 300 30
-
-# Static cursors
-single  left_ptr    0  3 point0040
-single  size_ver    8 40 holdcreature0042
-single  cross      31 32 possess
-single  whats_this  0 13 usedigger0041
-single  dnd-none    2  1 holdgold0040
-single  ibeam       0  0 dance0052
-single  forbidden   0 14 dance0044
-single  openhand    3 14 castspell0051
-single  closedhand  1  5 slap0051
-
-# Rotated cursors
-rotated size_hor   40 65 holdcreature0042 -90
-rotated size_fdiag 30 43 holdcreature0042 -45
-rotated size_bdiag  7 30 holdcreature0042  45
-
-rotated vertical-text  0 84 dance0052  -90
-
-# Aliases
-ln -sf size_hor       right_side
-ln -sf size_hor       left_side
-ln -sf size_hor       split_h
-ln -sf size_hor       sb_h_double_arrow
-
-ln -sf size_ver       bottom_side
-ln -sf size_ver       split_v
-ln -sf size_ver       top_side
-ln -sf size_ver       size_all
-ln -sf size_ver       fleur
-ln -sf size_ver       sb_v_double_arrow
-
-ln -sf cross          crosshair
-ln -sf cross          color-picker
-
-ln -sf pointing_hand  hand2
-ln -sf forbidden      not-allowed
-ln -sf left_ptr_watch progress
-ln -sf wait           watch
-ln -sf ibeam          xterm
-ln -sf whats_this     question_arrow
-
-ln -sf vertical-text  048008013003cff3c00c801001200000
-ln -sf zoom-in        f41c0e382c94c0958e07017e42b00462
-ln -sf zoom-out       f41c0e382c97c0938e07017e42800402
-ln -sf forbidden      03b6e0fcb3499374a867c041f52298f0
+if [ "${ERRORS}" -gt 0 ]; then
+    echo
+    echo "${ERRORS} errors occurred during parsing ${MAPPING_CONF} file."
+    echo "Please make sure that all the mentioned lines conform to the following format:"
+    echo
+    echo "name1 [name2, ...] x:y prefix frame1 [frame2, ...]"
+    echo
+fi
 
 # Build archive
 cd "${DIR}"
