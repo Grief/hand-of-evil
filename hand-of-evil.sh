b863112774274496e2f572019605b8c7a547d2ac
Grief <iamgrief@gmail.com>
2016-04-08 13:09:02 +0300

    added missing white border around conext-menu; publishing the code for preview generation

--- hand-of-evil.sh
+++ hand-of-evil.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 DIR="$(dirname "$(realpath "$0")")"
-ARCHIVE=HandOfEvil.zip
+CURSORS="${DIR}/hand-of-evil/cursors"
+PREVIEWS="${DIR}/previews"
+ARCHIVE="${DIR}/HandOfEvil.zip"
 TMP="${DIR}/tmp.png"
 TMP="${DIR}/tmp.png"
 MAPPING_CONF='mapping.conf'
@@ -25,7 +27,7 @@ function error() {
 }
 
 function process() {
-    tmp=$(printf "${DIR}/tmp%04d.png" "${index}")
+    tmp=$(printf "tmp%04d.png" "${index}")
     d=($(convert "${file}" -print '%W %H ' "${rotate_arg[@]}" "${do_args[@]}" "${effect_args[@]}" -trim -print '%X %Y %w %h %W %H' +repage "${tmp}" 2>&1))
     if [ -n "${rotate_arg}" ]; then
         hot_point=($(echo "${hot_point[@]} ${ANGLE} ${d[@]:0:2} ${d[@]:6:2}"|awk '{xd=$1-($4-1)/2; yd=$2-($5-1)/2; a=$3*atan2(0,-1)/180; c=cos(a); s=sin(a); printf("%.0f %.0f", ($6-1)/2+xd*c-yd*s, ($7-1)/2+yd*c+xd*s)}'))
@@ -62,9 +64,9 @@ function conf() {
         if [ -n "${BASH_REMATCH[3]}" ]; then delay="${BASH_REMATCH[3]}"; fi
         suffix="${BASH_REMATCH[1]}"
         if [ "${suffix}" = '*' ]; then
-            files=("$(printf "../../${FILE_MASK}" "${prefix}" '*')")
+            files=("$(printf "${FILE_MASK}" "${prefix}" '*')")
         else
-            files=("$(printf "../../${FILE_NAME}" "${prefix}" "${suffix}")")
+            files=("$(printf "${FILE_NAME}" "${prefix}" "${suffix}")")
         fi
         for file in ${files[@]}; do
             if [ -n "${processed["$file"]}" ]; then
@@ -92,7 +94,7 @@ function conf() {
     declare -A processed
     for ((f = 0; f < "${#config[@]}"; f++)); do
         args=(${config[f]})
-        tmp=$(printf "${DIR}/tmp%04d.png" "${args[4]}")
+        tmp=$(printf "tmp%04d.png" "${args[4]}")
         line="${size} ${max[2]} ${max[3]} ${tmp} ${args[5]}"
         echo "${line}"
         if [ -z "${processed["${args[4]}"]}" ]; then
@@ -103,6 +105,8 @@ function conf() {
 }
 
 function map() {
+    target_format=$1
+    shift
     unset name hot_point prefix frame
     if [[ $# -eq 0 || $1 =~ ^#.* ]]; then return; fi
     case $1 in
@@ -111,7 +115,7 @@ function map() {
         '!do') DO=("${@:2}"); return ;;
         '!rotate') ANGLE="$2"; return ;;
         '!effect') EFFECT=("${@:2}"); return ;;
-        '!alias') ln -sf "$3" "../../$2"; return ;;
+        '!alias') ln -sf "$3" "$2"; return ;;
         !*) error "Unknown option: ${1}"; return ;;
     esac
     for ((i = 1; i <= $#; i++)); do
@@ -138,65 +142,104 @@ function map() {
     config="$(conf "${@:i+1}")"
     code=$?
     if [ ${code} -eq 0 ]; then
-        echo "${config}"|xcursorgen - "${name}"
+        echo "    Generating ${name}..."
+        ${target_format}
         unset DO ANGLE
     else
         error "${MESSAGES[code]}"
     fi
 }
 
-# Download and unpack archive with images
-CURSORS="${DIR}/hand-of-evil/cursors"
-rm -r "${CURSORS}"
-mkdir -p "${CURSORS}"
-cd "${DIR}"
-if [ ! -f "${ARCHIVE}" ]; then
-    wget -q -O "${ARCHIVE}" --show-progress 'ftp://ftp.ea-europe.com/support/patches/dk2/HandOfEvil.zip'
-    if [ $? != 0 ]; then
-        echo "ERROR: Failed to download ${ARCHIVE} archive"
-        exit -1
+function generate() {
+    echo
+    if [ ! -f "${ARCHIVE}" ]; then
+        wget -q -O "${ARCHIVE}" --show-progress 'ftp://ftp.ea-europe.com/support/patches/dk2/HandOfEvil.zip'
+        if [ $? != 0 ]; then
+            echo "ERROR: Failed to download ${ARCHIVE} archive"
+            exit -1
+        fi
     fi
-fi
-if [ $(md5sum "${ARCHIVE}"|cut -d' ' -f1) != 'c1dd086f15a91bfa08c30530d0ff1e6f' ]; then
-    echo "ERROR: ${ARCHIVE} archive checksum mismatch"
-    exit -2
-fi
-unzip -q -o "${ARCHIVE}"
+    if [ $(md5sum "${ARCHIVE}"|cut -d' ' -f1) != 'c1dd086f15a91bfa08c30530d0ff1e6f' ]; then
+        echo "ERROR: ${ARCHIVE} archive checksum mismatch"
+        exit -2
+    fi
 
-# Prepare layout and create index file
-cd hand-of-evil
-echo "[Icon Theme]" > index.theme
-echo "Name=Hand of Evil" >> index.theme
-echo "Inherits=dummy-theme" >> index.theme
-cd cursors
+    rm -r "$1" 2> /dev/null
+    mkdir -p "$1"
+    cd "$1"
+    unzip -q -o "${ARCHIVE}"
 
-while read -ra words || [ -n "${words}" ]; do
-    map "${words[@]}"
-    let LINE++
-done < "${DIR}/${MAPPING_CONF}"
+    while read -ra words || [ -n "${words}" ]; do
+        map "$2" "${words[@]}"
+        let LINE++
+    done < "${DIR}/${MAPPING_CONF}"
 
-if [ "${ERRORS}" -gt 0 ]; then
-    echo
-    echo "${ERRORS} errors occurred during parsing ${MAPPING_CONF} file."
-    echo "Please make sure that all the mentioned lines conform to the following format:"
-    echo
-    echo "name1 [name2, ...] x:y prefix frame1 [frame2, ...]"
-    echo
-fi
+    if [ "${ERRORS}" -gt 0 ]; then
+        echo
+        echo "${ERRORS} errors occurred during parsing ${MAPPING_CONF} file."
+        echo "Please make sure that all the mentioned lines conform to the following format:"
+        echo
+        echo "name1 [name2, ...] x:y prefix frame1 [frame2, ...]"
+        echo
+    fi
+    echo "
+Generation completed."
+    rm *.png
+}
 
-# Build archive
-cd "${DIR}"
-tar czf hand-of-evil.tar.gz hand-of-evil
+function xcursor() {
+    echo "${config}"|xcursorgen - "${name}"
+}
 
-echo "
-GENERATION COMPLETED
+function gif() {
+    echo "${name}|![${name}](previews/${name}.gif)">>"${DIR}/previews.md"
+    unset cmd
+    while read -ra args
+    do
+        if [ -n "${args[4]}" ]; then
+            cmd+=("-delay" "$((args[4] / 10))")
+        fi
+        cmd+=("${args[3]}")
+    done < <(echo "${config}")
 
-You can now install the theme with one of the following ways:
+    convert -dispose Background "${cmd[@]}" -layers trim-bounds "${name}.gif"
+}
+
+case $1 in
+'xcursor')
+    echo 'Generating XCURSOR theme...'
+
+    generate "${CURSORS}" xcursor
+
+    cd ..
+    echo "[Icon Theme]" > index.theme
+    echo "Name=Hand of Evil" >> index.theme
+    echo "Inherits=core" >> index.theme
+
+    cd ..
+    tar czf hand-of-evil.tar.gz hand-of-evil
+
+    echo "You can now install the theme with one of the following ways:
+
 1. Using GUI, i.e. in KDE choose \"cursor theme\" from menu and install from:
-   ${DIR}/hand-of-evil.tar.gz
+    ${DIR}/hand-of-evil.tar.gz
 2. Manual way is to do:
-   sudo mv ${DIR}/hand-of-evil /usr/share/icons
-   sudo update-alternatives --install /usr/share/icons/default/index.theme x-cursor-theme /usr/share/icons/hand-of-evil/index.theme 200"
+    sudo mv ${DIR}/hand-of-evil /usr/share/icons
+    sudo update-alternatives --install /usr/share/icons/default/index.theme x-cursor-theme /usr/share/icons/hand-of-evil/index.theme 200"
+    exit
+    ;;
+'gif')
+    echo 'Generating GIF previews...'
+    echo "name|preview">"${DIR}/previews.md"
+    echo "---|---">>"${DIR}/previews.md"
 
-# Remove images
-rm *.png
+    generate "${PREVIEWS}" gif
+    find . -type l -exec rm {} \;
+    exit
+    ;;
+*)
+    echo "Unknown option: $1" >&2
+    ;;
+esac
+
+echo "Usage: $0 (xcursor | gif)"
